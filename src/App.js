862af1aac893ac8f083ec64582a14a6fe4772c78
toodimov <todor.dimov@hotmail.com>
2021-08-07 13:05:17 +0200

    setup env

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ import "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyASu_LZuvJN9Dj0gwcW2crf0p_QsNsP6hc",
-  authDomain: "react-chat-app-4f871.firebaseapp.com",
-  projectId: "react-chat-app-4f871",
-  storageBucket: "react-chat-app-4f871.appspot.com",
-  messagingSenderId: "1039829337230",
-  appId: "1:1039829337230:web:ae469a7efafa0e7a69f5df",
-});
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+};
+
+firebase.initializeApp(config);
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
